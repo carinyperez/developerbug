fec53a1c2ddd852790d4b09c38926fad3a14e0d3
Cariny Perez <perezcariny@gmail.com>
2021-02-23 19:08:08 -0800

    Addiding test user, auth,posts and profile routes

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require('express');
 const connectDB = require('./config/db'); 
 // creates an express application  
 const app = express(); 
-// const users = require('./routes/api/users'); 
-// const auth = require('./routes/api/auth');
-// const posts = require('./routes/api/posts');
-// const profile = require('./routes/api/profile'). 
+const users = require('./routes/api/users'); 
+const auth = require('./routes/api/auth');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 // Connect Database 
 connectDB(); 
 
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes 
-// app.use('/api/users',users); 
-// app.use('/api/auth', auth);
-// app.use('/api/posts', posts); 
-// app.use('/api/profile', profile);
+app.use('/api/users',users); 
+app.use('/api/auth', auth);
+app.use('/api/posts', posts); 
+app.use('/api/profile', profile);
 
 const PORT = process.env.PORT || 5000; 
 
