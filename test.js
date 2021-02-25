e4fee4aa1b0f0ee71c66474599e26acc03c65ae9
Cariny Perez <perezcariny@gmail.com>
2021-02-24 21:10:39 -0800

    Adding the profile model abd profile routes

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
+const profileFields = {}; 
+const skills = "HTML, CSS, PHP, Ruby"; 
 
+//{ skills: 'HTML,CSS,PHP,Ruby' }
+profileFields.skills = skills
+// { skills: [ 'HTML', 'CSS', 'PHP', 'Ruby' ] }
+.split(',')
+// trims white space 
+.map(skill => skill.trim())
 
-
+console.log(profileFields); 
