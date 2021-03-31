6a557ed5f300575f8f6ba25caa0039a1ea31bb71
cariny <perezcariny@gmail.com>
2021-03-30 20:57:45 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -7,19 +7,29 @@ const users = require('./routes/api/users');
 const auth = require('./routes/api/auth');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
+const path = require('path');
 // Connect Database 
 connectDB(); 
 // Init Middleware for body parser 
 //parses json and only looks at requests where the Content-Type header matches the type option
 app.use(express.json({extended: false}))
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes 
 app.use('/api/users',users); 
 app.use('/api/auth', auth);
 app.use('/api/posts', posts); 
 app.use('/api/profile', profile);
+
+//Serve static assets in production 
+if(process.env.NODE_ENV === 'production') {
+    //set static folder 
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000; 
 
